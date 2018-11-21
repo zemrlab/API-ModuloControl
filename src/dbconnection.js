f4e6fa6848dc792ab66a59f8cfa4ca0bc4743cab
zemrlab <zoraida.emperatriz@gmail.com>
2018-11-20 22:44:24 -0500

    Update dbconnection.js

--- src/dbconnection.js
+++ src/dbconnection.js
@@ -7,8 +7,10 @@ let pgp = require('pg-promise')(options);
 
 //const connectString = 'postgres://postgres:1234@localhost/recaudaciones';
 //const urlconnection = 'postgres://modulo4@sigap.postgres.database.azure.com:modulo4@sigap.postgres.database.azure.com:5432/tcs';
-const  urlconnection = 'postgres://modulo4:modulo4d@159.65.230.188:5432/tcs3';
-//const  urlconnection = 'postgres://modulo4:modulo4d@206.189.185.138:5432/tcs2';
+/*DESA*/
+//const  urlconnection = 'postgres://modulo4:modulo4d@159.65.230.188:5432/tcs3';
+/*PROD*/
+const  urlconnection = 'postgres://modulo4:modulo4d@206.189.185.138:5432/tcs2';
 let cn = pgp(urlconnection);
 
 module.exports = {
